7ef8913d6be75f068a640c16e726ef35bb4e6e68
Deadmanch <stepanova.nata.r@gmail.com>
2024-01-11 16:49:35 +0300

    123

--- 1-map/script.js
+++ 1-map/script.js
@@ -3,7 +3,7 @@
 TODO: Сделать с помощью Set уникализацию массива объектов
 ? Подсказка может понадобиться map и find
 */
-let users = [
+const users = [
 	{ id: 1, name: 'Вася' },
 	{ id: 2, name: 'Петя' },
 	{ id: 1, name: 'Вася' },
@@ -11,7 +11,7 @@ let users = [
 	{ id: 3, name: 'Данил' },
 	{ id: 4, name: 'Сергей' },
 ];
-let uniqueUsers = [...new Set(users.map(user => user.id))].map(id => {
+const uniqueUsers = [...new Set(users.map(user => user.id))].map(id => {
 	return users.find(user => user.id === id);
 });
 
