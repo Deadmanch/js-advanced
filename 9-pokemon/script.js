511e7ed8ac4954780296ffd4958797e601115173
Deadmanch <stepanova.nata.r@gmail.com>
2024-01-26 14:35:38 +0300

    Добавлена обработка события error

--- 9-pokemon/script.js
+++ 9-pokemon/script.js
@@ -1,16 +1,17 @@
 'use strict';
-
 const request = new XMLHttpRequest();
-request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+request.open('GET', 'https://poke2api.co/api/v2/pokemon/ditto');
 request.send();
-
 request.addEventListener('load', function () {
 	const { abilities } = JSON.parse(this.responseText);
 	const request = new XMLHttpRequest();
-	request.open('GET', `${abilities[0].ability.url}`);
+	request.open('GET', `${abilities[1].ability.url}`);
 	request.send();
 	request.addEventListener('load', function () {
 		const { effect_entries } = JSON.parse(this.responseText);
 		console.log(effect_entries[1].effect);
 	});
+});
+request.addEventListener('error', function () {
+	console.log('Ошибка при получении данных');
 });
